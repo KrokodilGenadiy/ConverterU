9d133983d31791e895765e651cc9aea69f5a4795
KrokodilGenadiy <alex.usevich@gmail.com>
2022-02-27 14:52:48 +0300

    Initial commit

---
+++ app/src/main/java/com/zaus_app/converter/data/MainRepository.kt
@@ -0,0 +1,10 @@
+package com.zaus_app.converter.data
+
+import com.zaus_app.converter.data.entity.Valute
+
+class MainRepository {
+    val valutes = listOf(
+        Valute("1", "ss", "ss", 1, "BY", 1.2, 1.1),
+        Valute("2", "bb", "bb", 2, "RU", 12.2, 12.1)
+    )
+}
---
+++ app/src/main/java/com/zaus_app/converter/data/entity/Valute.kt
@@ -0,0 +1,11 @@
+package com.zaus_app.converter.data.entity
+
+data class Valute(
+    val ID: String,
+    val NumCode: String,
+    val CharCode: String,
+    val Nominal: Int,
+    val Name: String,
+    val Value: Double,
+    val Previous: Double
+)
