4d991b5e26f368746d78986ca05b9366e2a50f8e
KrokodilGenadiy <alex.usevich@gmail.com>
2022-03-05 02:39:00 +0300

    fixed some errors

--- app/src/main/java/com/zaus_app/converter/view/fragments/ConvertFragment.kt
+++ app/src/main/java/com/zaus_app/converter/view/fragments/ConvertFragment.kt
@@ -1,16 +1,17 @@
 package com.zaus_app.converter.view.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.zaus_app.converter.data.entity.Currency
 import com.zaus_app.converter.databinding.FragmentConvertBinding
 import com.zaus_app.converter.viewmodel.ConvertFragmentViewModel
+import java.math.BigInteger
 
 
 class ConvertFragment : Fragment() {
@@ -49,7 +50,13 @@ class ConvertFragment : Fragment() {
     fun convert() {
         val nominalRus = binding.valuteInput1.text
         if (binding.valuteInput1.text.isNullOrEmpty()) {
-            Toast.makeText(activity,TOAST_TEXT, Toast.LENGTH_LONG).show()
+            Toast.makeText(activity,ENTER_CURRENCY_TEXT, Toast.LENGTH_LONG).show()
+            return
+        }
+        val maxInt: BigInteger = BigInteger.valueOf(Int.MAX_VALUE.toLong())
+        val value = BigInteger(nominalRus.toString())
+        if (value > maxInt) {
+            Toast.makeText(activity,BIG_NUMBER, Toast.LENGTH_LONG).show()
             return
         }
         viewModel.convert(nominalRus.toString().toInt(),currency.Value,currency.Nominal)
@@ -57,7 +64,8 @@ class ConvertFragment : Fragment() {
     }
 
     companion object {
-        const val TOAST_TEXT = "Введите кол-во валюты"
+        const val ENTER_CURRENCY_TEXT = "Введите кол-во валюты"
+        const val BIG_NUMBER = "Номинал слишком большой"
     }
 
 }
